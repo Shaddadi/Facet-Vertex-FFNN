be478db08381eb82be355348a22e61b4f64a37ae
Xiaodong Yang <32499911+Shaddadi@users.noreply.github.com>
2021-01-14 15:21:35 -0500

    Update property5-10.sh

--- examples/ACASXu/property5-10.sh
+++ examples/ACASXu/property5-10.sh
@@ -8,8 +8,8 @@ timeout --foreground --signal=SIGQUIT $TIMEOUT python3 main.py --property 6.1 --
 sleep 1.0
 timeout --foreground --signal=SIGQUIT $TIMEOUT python3 main.py --property 6.2 --n1 1 --n2 1
 sleep 1.0
-timeout --foreground --signal=SIGQUIT $TIMEOUT python3 main.py --property 7 --n1 1 --n2 9
-sleep 1.0
+#timeout --foreground --signal=SIGQUIT $TIMEOUT python3 main.py --property 7 --n1 1 --n2 9
+#sleep 1.0
 timeout --foreground --signal=SIGQUIT $TIMEOUT python3 main.py --property 8 --n1 2 --n2 9
 sleep 1.0
 timeout --foreground --signal=SIGQUIT $TIMEOUT python3 main.py --property 9 --n1 3 --n2 3
